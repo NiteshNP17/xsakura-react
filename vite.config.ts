5fec79b560c03711d9c26721a164181bdb8d8e28
NiteshNP17 <niteshpatel107@gmail.com>
2024-06-14 00:36:06 +0530

    downgraded from React 19 RC back to React 18.3.1

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
-const ReactCompilerConfig = {
-  /* ... */
-};
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
-      },
-    }),
+    react({}),
     VitePWA({
       registerType: "autoUpdate",
       injectRegister: "auto",
