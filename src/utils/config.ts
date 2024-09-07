ce2f7082deeba87861c2daa4e3443e38f352bf1d
NiteshNP17 <niteshpatel107@gmail.com>
2024-09-07 16:48:51 +0530

    switched config to listen to live server

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -5,8 +5,8 @@ interface Config {
 }
 
 const config: Config = {
-  apiUrl: "http://localhost:5000",
-  // apiUrl: import.meta.env.VITE_API_URL || "http://localhost:5000",
+  // apiUrl: "http://localhost:5000",
+  apiUrl: import.meta.env.VITE_API_URL || "http://localhost:5000",
 };
 
 export default config;
